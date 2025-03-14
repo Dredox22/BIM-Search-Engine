979a922115bad33d659cdbd2c41d8bdefc23b45a
Konstantin <dredox@gmail.com>
2025-03-14 18:33:39 +0300

    edit app_v2.py add parsing optimisation

--- app_v2.py
+++ app_v2.py
@@ -14,10 +14,23 @@ SYNONYMS = {
     "окна": "окно"
 }
 
-# Парсим BIM-модель
+def load_bim_model(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    bim_dict = {}
+    for floor in root.findall(".//floor"):
+        floor_num = floor.get("number")
+        bim_dict[floor_num] = []
+        for elem in floor:
+            bim_dict[floor_num].append({
+                "id": elem.get("id"),
+                "type": elem.get("type"),
+                "material": elem.get("material")
+            })
+    return bim_dict
 
-tree = ET.parse('bim_model_v2.xml')
-root = tree.getroot()
+# Загружаем модель один раз
+BIM_MODEL = load_bim_model('bim_model_v2.xml')
 
 def search_bim(query):
     doc = nlp(query.lower())
@@ -25,32 +38,23 @@ def search_bim(query):
     element_type = None
     material = None
     
-    # Извлекаем сущности
-    
     for token in doc:
         if token.text.isdigit():
             floor_num = token.text
         elif token.lemma_ in SYNONYMS:
             element_type = SYNONYMS[token.lemma_]
-        elif token.lemma_ in ["бетон", "дерево", "сталь"]:  # Материалы
+        elif token.lemma_ in ["бетон", "дерево", "сталь"]:
             material = token.lemma_
     
-    # Поиск по дереву
-    
     results = []
-    for floor in root.findall(".//floor"):
-        if floor_num and floor.get("number") != floor_num:
+    for floor, elements in BIM_MODEL.items():
+        if floor_num and floor != floor_num:
             continue
-        for elem in floor:
-            if element_type and elem.get("type") == element_type:
-                if material and elem.get("material") != material:
+        for elem in elements:
+            if element_type and elem["type"] == element_type:
+                if material and elem["material"] != material:
                     continue
-                results.append({
-                    "id": elem.get("id"),
-                    "floor": floor.get("number"),
-                    "type": elem.get("type"),
-                    "material": elem.get("material")
-                })
+                results.append(elem)
     
     return results if results else {"error": "Ничего не найдено"}
 
